bba5939fa9fb97cc1568a743abdf993d7fd56a9e
Eric Marshall <emm035@bucknell.edu>
2017-09-27 09:51:38 -0400

    Created a SIGN_EXTEND module
    Sign-extends a value from 16 bits to 32 bits, such that all of the leading
    bits that are prepended match the leftmost bit of the input value. Resolves #5

---
+++ modules/SIGN_EXTEND/SIGN_EXTEND.v
@@ -0,0 +1,13 @@
+/*
+ * Sign-extends a value from 16 bits to 32 bits, such that all of the leading
+ * bits that are prepended match the leftmost bit of the input value
+ */
+module SIGN_EXTEND (sign_in, sign_out);
+
+  input wire [15:0] sign_in;
+  input wire [31:0] sign_out;
+
+  // Prepends the leftmost bit of sign_in 16x
+  assign sign_out = {{16{sign_in[15]}}, sign_in};
+
+endmodule
