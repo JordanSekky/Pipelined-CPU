d7ec318f11cca2b47bfaaa8fa77d59caa67d8656
Eric Marshall <emm035@bucknell.edu>
2017-10-01 11:08:18 -0400

    Minor fixes to ADDER tests & added output.txt
    Resolves #26

--- modules/ADDER.v
+++ modules/ADDER.v
@@ -1,3 +1,3 @@
-module ADDER (input [31:0] input_a, input [31:0] input_b, output [31:0] result) begin 
+module ADDER (input [31:0] input_a, input [31:0] input_b, output [31:0] result);
   assign result = input_a + input_b;
-end
+endmodule
