a8e2ff8d89b28e0f0dc20597ac7cac7a9424b27a
Eric Marshall <emm035@bucknell.edu>
2017-10-05 09:15:13 -0400

    Added Data Memory tests with printing

--- tests/DATA_MEMORY/test.v
+++ tests/DATA_MEMORY/test.v
@@ -61,7 +61,7 @@ module testbench();
     #10; // 00000000
 
     write_data = 32'h0000dead;
-    addr = 0;
+    addr = addr-1;
     sig_mem_write = 1'b0;
     #10; // 00000000
 
@@ -71,16 +71,19 @@ module testbench();
     #10; // 00000000
     
     // Test printing strings. 
-    addr = addr - 10;
+    addr <= addr - 10;
     temp_print_string_address = addr;
-    write_data = 32'h68656c; // "hell"
-    sig_mem_write = 1'b1;
+    write_data <= 32'h68656c6c; // "hell"
+    sig_mem_write <= 1'b1;
     #5;
-    addr = addr - 1;
-    write_data = 32'h6f20776f; // "o wo"
+    addr <= addr + 1;
+    write_data <= 32'h6f20776f; // "o wo"
     #5;
-    addr = addr - 1;
-    write_data = 32'h726c6400; // "rld";
+    addr <= addr + 1;
+    write_data <= 32'h726c6400; // "rld";
+    #5;
+    print_string_m <= addr-2; // Print
+    #10;
 
     $finish;
   end
