3d7f5abc2b3f4837a7bbf977b4331cc98bd4c650
Eric Marshall <emm035@bucknell.edu>
2017-10-15 11:12:36 -0400

    Fixed string printing in memory module

--- modules/MEMORY.v
+++ modules/MEMORY.v
@@ -20,11 +20,11 @@ module MEMORY (
 
   initial begin
     `ifndef TEST_H
-      $readmemh("../mips/fibonacci_test/fibonacci_test.bin", text);
+      $readmemh("../mips/print_string_test/print_string_test.bin", text);
     `endif
   end
 
-  always @(data_addr or instr_pc) begin
+  always @(data_addr or instr_pc or data_print_addr) begin
     if (((data_addr) < `stack_size_lo) || ((data_addr) > `stack_size_hi))
       $display("stack address %x out of bounds.", (data_addr));
     if (((instr_pc) < `text_size_lo) || ((instr_pc) > `text_size_hi))
@@ -54,32 +54,34 @@ module MEMORY (
   end
 
   // Print strings from data memory
-
-  reg [1:0] byte_offset = 0;
-  reg [31:0] word_offset = 0;
-  reg [7:0] char;
+  reg  [1:0]  byte_offset = 0;
+  reg  [31:0] word_offset = 0;
+  reg  [7:0]  char;
   wire [31:0] word;
+  wire [31:0] data_print_addr_shifted;
 
-  assign data_print_addr_shifted = data_print_addr_shifted >> 2;
+  assign data_print_addr_shifted = data_print_addr >> 2;
   assign word = text[data_print_addr_shifted + word_offset];
 
   integer i;
-  always @(data_print_addr_shifted) begin
+  always @(data_print_addr) begin
     if (data_print_addr_shifted > 0) begin
       word_offset = 0;
       byte_offset = 0;
       char = word[31:24];
+      $display("\n\n\n\n\n\n");
       while (char != 8'h00) begin
         case (byte_offset)
-          0: char = word[31:24];
-          1: char = word[23:16];
-          2: char = word[15:8];
-          3: char = word[7:0];
+          3: char = word[31:24];
+          2: char = word[23:16];
+          1: char = word[15:8];
+          0: char = word[7:0];
         endcase
         if (char != 8'h00) $write("%c", char);
         if (byte_offset == 3) word_offset += 1;
         byte_offset += 1;
       end
+      $display("\n\n\n\n\n\n");
       $display();
     end
   end
